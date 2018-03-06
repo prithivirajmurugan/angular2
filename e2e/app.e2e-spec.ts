2903077a0b45222e0ef7142f3fb565121786328b
Prithiviraj <prithivist@gmail.com>
2018-03-06 15:42:01 +0530

    Angular end to end Test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('con-fusion App', () => {
   let page: AppPage;
@@ -7,8 +8,30 @@ describe('con-fusion App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display message saying Ristorante Con Fusion', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
   });
+  it('should navigate to about us page by clicking the link',()=>
+      {
+ page.navigateTo('/');
+ let navLink= page.getAllElements('a').get(1);
+ navLink.click();
+
+expect(page.getParagraphText('h3')).toBe('About Us');
+      });
+
+it('should enter a new comment for the first dish',()=>
+   {
+     page.navigateTo("/dishdetail/0");
+     let newAuthor=page.getElement('input[type=text]');
+     newAuthor.sendKeys('Test Author');
+      let newRating=page.getElement('mat-slider');
+      newRating.sendKeys(5);
+     let newComment=page.getElement('textarea');
+     newComment.sendKeys('Test Comment');
+     let newSubmitButton=page.getElement('button[type=submit]');
+     newSubmitButton.click();
+     browser.pause();
+   });  
 });
